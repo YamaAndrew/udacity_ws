bb501fb7b50bc84da3d37e8b58f68256b6fea283
YamaAndrew <andrew926yamasaki@gmail.com>
2025-05-16 18:25:07 -0700

    add my_robot

---
+++ Project3/catkin_ws/build/pgm_map_creator/msgs/CMakeFiles/collision_map_creator_msgs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "collision_map_request.pb.cc"
+  "collision_map_request.pb.h"
+  "vector2d.pb.cc"
+  "vector2d.pb.h"
+  "CMakeFiles/collision_map_creator_msgs.dir/collision_map_request.pb.cc.o"
+  "CMakeFiles/collision_map_creator_msgs.dir/vector2d.pb.cc.o"
+  "/home/robond/udacity_ws/Project3/catkin_ws/devel/lib/libcollision_map_creator_msgs.pdb"
+  "/home/robond/udacity_ws/Project3/catkin_ws/devel/lib/libcollision_map_creator_msgs.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/collision_map_creator_msgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
